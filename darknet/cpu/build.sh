8104256a359c9d49497020a7c4eab4bc9b03b351
a.svetlitskiy <a.svetlitskiy@gmail.com>
2019-03-20 11:23:31 +0300

    fix bugs

--- darknet/cpu/build.sh
+++ darknet/cpu/build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # Darkenet AB CPU
 #
@@ -7,4 +8,4 @@
 # Copyright (c) 2019 Aleksey Svetlitskiy - https://github.com/svetlitskiy/docker
 #
 IMAGE=darknet_cpu
-cd ../../opencv3/ && ./build.sh && cd ../darknet/cpu && docker build -t $IMAGE .
+./build.sh && cd ../darknet/cpu && docker build -t $IMAGE .
--- darknet/gpu/run.sh
+++ darknet/gpu/run.sh
@@ -7,11 +7,13 @@
 #
 
 IMAGE=darknet_gpu
-if [ $1="train" ]; then
-CMD=./train.sh
-else
-CMD=bash
-fi
+#if [ $1="train" ]; then
+#CMD=./train.sh
+#else
+#CMD=bash
+#fi
 
-docker run --rm -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 -v nvidia_driver_384.66:/usr/local/nvidia:ro --name $IMAGE $IMAGE $CMD
+#docker run --rm -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 -v nvidia_driver_384.66:/usr/local/nvidia:ro --name $IMAGE $IMAGE $CMD
+
+docker run -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/:/root/home --rm -it --device=/dev/nvidiactl --device=/dev/nvidia0 -v nvidia_driver_384.66:/usr/local/nvidia:ro --name $IMAGE $IMAGE bash
 
